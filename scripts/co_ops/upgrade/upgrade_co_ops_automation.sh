0582a414ab907d0393c68c4ec186f23f46e57468
maxfan2023 <126193330+maxfan2023@users.noreply.github.com>
2026-04-13 17:34:43 +0800

    Skip step 9 outside denv

--- scripts/co_ops/upgrade/upgrade_co_ops_automation.sh
+++ scripts/co_ops/upgrade/upgrade_co_ops_automation.sh
@@ -789,18 +789,16 @@ step_8_update_abinitiorc_java_home() {
 step_9_validate_batch_profile() {
   local shell_cmd=""
 
-  if [[ "${ENV_NAME}" == "denv" ]]; then
-    shell_cmd="dzdo /bin/su - ${BATCH_USER} <<'AB_BATCH_VALIDATE'
+  if [[ "${ENV_NAME}" != "denv" ]]; then
+    log INFO "step_9 is skipped for ${ENV_NAME}; batch-account validation is only required in denv"
+    return 0
+  fi
+
+  shell_cmd="dzdo /bin/su - ${BATCH_USER} <<'AB_BATCH_VALIDATE'
 source ${TARGET_PROFILE}
 installation-test
 ab-key show
 AB_BATCH_VALIDATE"
-  else
-    printf -v shell_cmd \
-      'source %q && installation-test && ab-key show' \
-      "${TARGET_PROFILE}"
-  fi
-
   run_shell_cmd "${shell_cmd}"
 }
 
